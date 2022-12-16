4b3ca358822d2579b16815ae68ce040e82304909
manueljenni <manuelnoahjenni@outlook.com>
2022-12-16 22:11:49 +0100

    Update article generation

--- app/articles/[articleId]/page.tsx
+++ app/articles/[articleId]/page.tsx
@@ -1,53 +1,53 @@
-import React from 'react'
-import { remark } from 'remark';
-import remarkHtml from 'remark-html';
-import path from 'path';
-import { promises as fs } from 'fs';
-import fetchData from '../../fetchData';
-import Link from 'next/link';
+import React from "react";
+import { remark } from "remark";
+import remarkHtml from "remark-html";
+import path from "path";
+import { promises as fs } from "fs";
+import fetchData from "../../fetchData";
+import Link from "next/link";
 
 export default async function page(props: any) {
-    var article = await fetchData.getArticleById(props.params.articleId);
-    var html = await getMarkdownAsHtml("test");
-    var tags = article.tags.split(',');
+  var article = await fetchData.getArticleById(props.params.articleId);
+  var html = await getMarkdownAsHtml("test");
+  var tags = article.tags.split(",");
 
-    return (
-        <div>
-            <div className='text-center centerDiv'>
-                <div className='space-y-6 mb-12'>
-                    <div className='space-y-4'>
-                        <h1 className='text-4xl font-medium'>{article.title}</h1>
-                        <h2 className='text-2xl'>{article.subtitle}</h2>
-                    </div>
-                    <div className="centerDiv">
-                        <div className='flex space-x-3 text-white text-sm'>
-                            {tags.map((tag: string) =>
-                                <div className='py-1 px-2 bg-main rounded-lg'><p>{'#' + tag}</p></div>
-                            )}
-                        </div>
-                    </div>
+  return (
+    <div>
+      <div className="text-center centerDiv">
+        <div className="space-y-6 mb-12">
+          <div className="space-y-4">
+            <h1 className="text-4xl font-medium">{article.title}</h1>
+            <h2 className="text-2xl">{article.subtitle}</h2>
+          </div>
+          <div className="centerDiv">
+            <div className="flex space-x-3 text-white text-sm">
+              {tags.map((tag: string) => (
+                <div className="py-1 px-2 bg-main rounded-lg">
+                  <p>{"#" + tag}</p>
                 </div>
+              ))}
             </div>
-            <article className="prose" dangerouslySetInnerHTML={{ __html: html }} />
-            <Link href={"/articles"}><p className='mb-4 accentText'>← All articles</p></Link >
-        </div >
-    )
+          </div>
+        </div>
+      </div>
+      <article className="prose" dangerouslySetInnerHTML={{ __html: html }} />
+      <Link href={"/articles"}>
+        <p className="mb-4 accentText">← All articles</p>
+      </Link>
+    </div>
+  );
 }
 
 async function getMarkdownAsHtml(fileName: string) {
-    var filePath = path.join(process.cwd(), '/app/articles/md', fileName + '.md');
-    var content = await fs.readFile(filePath, 'utf8');
+  var filePath = path.join(process.cwd(), "/app/articles/md", fileName + ".md");
+  var content = await fs.readFile(filePath, "utf8");
 
-    const processedContent = await remark()
-        .use(remarkHtml)
-        .process(content);
-
-    return processedContent.toString();
+  return await (await remark().use(remarkHtml).process(content)).toString();
 }
 
 export const generateStaticParams = async () => {
-    var allArticles = await fetchData.fetchArticlesSummary();
-    return allArticles.map((article: any) => ({
-        articleId: article.id.toString(),
-    }));
-}
+  var allArticles = await fetchData.fetchArticlesSummary();
+  return allArticles.map((article: any) => ({
+    articleId: article.id.toString(),
+  }));
+};
