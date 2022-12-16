7c585c1151d8e761bcc509c350ca595744a12d58
manueljenni <97627960+manueljenni@users.noreply.github.com>
2022-12-16 13:44:48 +0100

    Update footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,13 @@ import Footer from "./Footer";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" >
       <head />
-      <body className="bg-neutral-50 body space-y-12 my-8 min-h-screen">
-        <Header />
-        {children}
+      <body className="bg-neutral-50 body space-y-12 my-8 flex flex-col justify-between">
+        <section className="m-0 p-0 space-y-12">
+          <Header />
+          {children}
+        </section>
         <Footer />
       </body>
     </html>
