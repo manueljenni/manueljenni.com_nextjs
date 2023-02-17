4a6ecac302500d1b0bcd92810f742aa4f7e9c30f
manueljenni <manuelnoahjenni@outlook.com>
2023-02-17 10:24:47 +0000

    Add clock

--- app/Components/CurrentStats.tsx
+++ app/Components/CurrentStats.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import BoxThree from "./BoxThree";
 import Utils from "../Utils";
 
@@ -6,6 +7,10 @@ export default function CurrentStats(props: any) {
   const nextFlight = props.nextFlight;
   const currentLocation = props.currentLocation;
   const pastFlight = props.pastFlight;
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    setInterval(() => setTime(new Date()), 500);
+  }, []);
 
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -39,9 +44,7 @@ export default function CurrentStats(props: any) {
         )}
         <BoxThree
           top="Manuel's current time is"
-          main={
-            new Date().toLocaleTimeString() + " " + Utils.getCurrentTimeEmoji(new Date())
-          }
+          main={time.toLocaleTimeString() + " " + Utils.getCurrentTimeEmoji(new Date())}
           bottom={"in " + currentLocation.city}
         />
       </>
