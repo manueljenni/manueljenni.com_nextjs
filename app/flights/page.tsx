6fd13441bd045b2a4cf800fa709af9aac658dd94
manueljenni <manuelnoahjenni@outlook.com>
2023-04-01 17:37:29 +0100

    Update dependencies

--- app/flights/page.tsx
+++ app/flights/page.tsx
@@ -41,7 +41,7 @@ export default async function page() {
             <FlightsTable flights={upcomingFlights} />
           </div>
         )}
-        <div className="rounded-lg border overflow-hidden w-full">
+        <div className="w-full overflow-hidden rounded-lg border">
           <Map
             locations={locations}
             upcomingRoutes={upcomingRoutes}
